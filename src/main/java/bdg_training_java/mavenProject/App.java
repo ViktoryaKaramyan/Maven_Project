40e8bfd1075886aa1be4942cb2f63d6d3b98b43c
Viktorya <viktorya.karamyan@betconstruct.com>
2019-04-02 15:23:52 +0400

    Maven Project

---
+++ src/main/java/bdg_training_java/mavenProject/App.java
@@ -0,0 +1,13 @@
+package bdg_training_java.mavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
